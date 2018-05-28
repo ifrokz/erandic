f589cf89f94d76aa0532b71c446eb8426c59375b
ifrokz <dyfrok@gmail.com>
2018-05-28 16:49:05 +0200

    New: redux store (react-redux, redux, redux-thunk, redux-devtools-extension)

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,64 @@
 import '../components/styles/style.scss';
+import {connect} from 'react-redux'
+import {startClock, serverRenderClock} from '../redux/store'
 
 const isClientOrServer = () => {
   return (typeof window !== 'undefined' && window.document) ? 'client' : 'server';
 };
 
-const Index = ({userAgent, params, query}) => (
-  <div>
-    <h1>Hello world from Next JS {isClientOrServer()}</h1>
+// const Index = ({userAgent, params, query}) => {
+//   console.log(params);return(
+//   <div>
+//     <h1>Hello world from Next JS {isClientOrServer()}</h1>
+//     <div>
+//       <p>{userAgent}</p>
+//       <p>{JSON.stringify(params)}</p>
+//       <p>{JSON.stringify(query)}</p>
+//     </div>
+//   </div>
+// )};
+
+// Index.getInitialProps = async ({req}) => {
+//   const userAgent = req ? req.headers['user-agent'] : navigator.userAgent
+//   console.log('body:', req.query, req.query, req.params);
+//   return { userAgent, params: req.params, query: req.query }
+// }
+
+class Index extends React.Component {
+  static getInitialProps = async ({reduxStore, req}) => {
+    const isServer = !!req
+    reduxStore.dispatch(serverRenderClock(isServer))
+
+    const userAgent = req ? req.headers['user-agent'] : navigator.userAgent
+    console.log('body:', req.query, req.query, req.params);
+    return { userAgent, params: req.params, query: req.query }
+  }
+
+  componentDidMount () {
+    const {dispatch} = this.props
+    this.timer = startClock(dispatch)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.timer)
+  }
+
+  render = () => (
     <div>
-      <p>{userAgent}</p>
-      <p>{JSON.stringify(params)}</p>
-      <p>{JSON.stringify(query)}</p>
+      <h1>Hello world from Next JS {isClientOrServer()}</h1>
+      <div>
+        <p>{`userAgent => ${this.props.userAgent}`}</p>
+        <p>{`Params => ${JSON.stringify(this.props.params)}`}</p>
+        <p>{`Query => ${JSON.stringify(this.props.query)}`}</p>
+        <p>{this.props.lastUpdate}</p>
+      </div>
     </div>
-  </div>
-);
-
-Index.getInitialProps = async ({req}) => {
-  const userAgent = req ? req.headers['user-agent'] : navigator.userAgent
-  console.log('body:', req.query, req.query, req.params);
-  return { userAgent, params: req.params, query: req.query }
+  );
+};
+function mapStateToProps (state) {
+  console.log(state)
+  const {lastUpdate} = state
+  return {lastUpdate}
 }
-
-export  default Index;
+export default connect(mapStateToProps)(Index)
+// export  default Index;
