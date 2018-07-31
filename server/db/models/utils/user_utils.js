80df4671deb1c04b2b7de976f5f941b5718bb37f
Ivan Ruiz <dyfrok@gmail.com>
2018-07-31 12:37:42 +0200

    Update genPayload 2

--- server/db/models/utils/user_utils.js
+++ server/db/models/utils/user_utils.js
@@ -7,7 +7,7 @@ const genPayload = ({_id, access, expTime}) => {
       access: access, 
       _id: _id.toHexString(),
       iat: moment.now() / 1000,
-      exp: expTime ? expTime :  moment().add(1, 'h')
+      exp: expTime ? expTime :  moment().add(1, 'h').valueOf() / 1000
   };
 
   return payload;
